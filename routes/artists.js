8e40ce33d71f171b17548d040701cb11479a5a25
Anton Morozov <symfosium@gmail.com>
2024-12-10 12:02:12 +0200

    artists.js updated

--- routes/artists.js
+++ routes/artists.js
@@ -3,128 +3,192 @@ const router = express.Router();
 
 // Mock data for artists
 const artists = [
-    {
-        name: 'Anna Katariina',
-        description: 'Lyhyt kuvaus Anna Katariinasta.',
-        imageUrl: '/images/artists_jpg.jpg',
-        moreInfoUrl: '/artists/anna-katariina',
-        facebookUrl: 'https://facebook.com',
-        instagramUrl: 'https://instagram.com',
-        youtubeUrl: 'https://youtube.com',
-    },
-    {
-        name: 'Duo Songbirds',
-        description: 'Lyhyt kuvaus Duo Songbirdsistä.',
-        imageUrl: '/images/artists_jpg.jpg',
-        moreInfoUrl: '/artists/duo-songbirds',
-        facebookUrl: 'https://facebook.com',
-        instagramUrl: 'https://instagram.com',
-        youtubeUrl: 'https://youtube.com',
-    },
-    {
-        name: 'Saksofonisti Anton Morozov',
-        description: 'Lyhyt kuvaus Anton Morozovista.',
-        imageUrl: '/images/artists_jpg.jpg',
-        moreInfoUrl: '/artists/anton-morozov',
-        facebookUrl: 'https://facebook.com',
-        instagramUrl: 'https://instagram.com',
-        youtubeUrl: 'https://youtube.com',
-    },
-    {
-        name: 'Tytti Koivunen',
-        description: 'Lyhyt kuvaus Tytti Koivusesta.',
-        imageUrl: '/images/artists_jpg.jpg',
-        moreInfoUrl: '/artists/tytti-koivunen',
-        facebookUrl: 'https://facebook.com',
-        instagramUrl: 'https://instagram.com',
-        youtubeUrl: 'https://youtube.com',
-    },
-    {
-        name: 'Lotta Virkkunen',
-        description: 'Lyhyt kuvaus Lotta Virkkusesta.',
-        imageUrl: '/images/artists_jpg.jpg',
-        moreInfoUrl: '/artists/lotta-virkkunen',
-        facebookUrl: 'https://facebook.com',
-        instagramUrl: 'https://instagram.com',
-        youtubeUrl: 'https://youtube.com',
-    },
-    {
-        name: 'Tanja Vähäsarja',
-        description: 'Lyhyt kuvaus Tanja Vähäsarjasta.',
-        imageUrl: '/images/artists_jpg.jpg',
-        moreInfoUrl: '/artists/tanja-vahasarja',
-        facebookUrl: 'https://facebook.com',
-        instagramUrl: 'https://instagram.com',
-        youtubeUrl: 'https://youtube.com',
-    },
-    {
-        name: 'Juontaja Kimmo Oksanen',
-        description: 'Lyhyt kuvaus Kimmo Oksasesta.',
-        imageUrl: '/images/artists_jpg.jpg',
-        moreInfoUrl: '/artists/kimmo-oksanen',
-        facebookUrl: 'https://facebook.com',
-        instagramUrl: 'https://instagram.com',
-        youtubeUrl: 'https://youtube.com',
-    },
-    {
-        name: 'Toni Jokiniitty',
-        description: 'Lyhyt kuvaus Toni Jokiniitystä.',
-        imageUrl: '/images/artists_jpg.jpg',
-        moreInfoUrl: '/artists/toni-jokiniitty',
-        facebookUrl: 'https://facebook.com',
-        instagramUrl: 'https://instagram.com',
-        youtubeUrl: 'https://youtube.com',
-    },
-    {
-        name: 'Joonas Eloranta',
-        description: 'Lyhyt kuvaus Joonas Elorannasta.',
-        imageUrl: '/images/artists_jpg.jpg',
-        moreInfoUrl: '/artists/joonas-eloranta',
-        facebookUrl: 'https://facebook.com',
-        instagramUrl: 'https://instagram.com',
-        youtubeUrl: 'https://youtube.com',
-    },
-    {
-        name: 'PUSHKIN Quintett',
-        description: 'Lyhyt kuvaus PUSHKIN Quintetista.',
-        imageUrl: '/images/artists_jpg.jpg',
-        moreInfoUrl: '/artists/pushkin-quintett',
-        facebookUrl: 'https://facebook.com',
-        instagramUrl: 'https://instagram.com',
-        youtubeUrl: 'https://youtube.com',
-    },
-    {
-        name: 'Night Shift',
-        description: 'Lyhyt kuvaus Night Shiftistä.',
-        imageUrl: '/images/artists_jpg.jpg',
-        moreInfoUrl: '/artists/night-shift',
-        facebookUrl: 'https://facebook.com',
-        instagramUrl: 'https://instagram.com',
-        youtubeUrl: 'https://youtube.com',
-    },
-    {
-        name: 'Henriikka Roo',
-        description: 'Lyhyt kuvaus Henriikka Roosta.',
-        imageUrl: '/images/artists_jpg.jpg',
-        moreInfoUrl: '/artists/henriikka-roo',
-        facebookUrl: 'https://facebook.com',
-        instagramUrl: 'https://instagram.com',
-        youtubeUrl: 'https://youtube.com',
-    },
-    {
-        name: 'Tampereen Ukuleleorkesteri',
-        description: 'Lyhyt kuvaus Tampereen Ukuleleorkesterista.',
-        imageUrl: '/images/artists_jpg.jpg',
-        moreInfoUrl: '/artists/tampereen-ukuleleorkesteri',
-        facebookUrl: 'https://facebook.com',
-        instagramUrl: 'https://instagram.com',
-        youtubeUrl: 'https://youtube.com',
-    },
-];
+  {
+    id: 1,
+    name: 'Anna Katariina',
+    description: 'Lyhyt kuvaus Anna Katariinasta.',
+    imageUrl: '/images/artists_jpg.jpg',
+    facebookUrl: 'https://facebook.com',
+    instagramUrl: 'https://instagram.com',
+    youtubeUrl: 'https://youtube.com',
+    videos: [
+      'https://www.youtube.com/embed/DCopcNpzc60',
+      'https://www.youtube.com/embed/oGlDJ3GxvLc',
+    ],
+  },
+  {
+    id: 2,
+    name: 'Duo Songbirds',
+    description: 'Lyhyt kuvaus Duo Songbirdsistä.',
+    imageUrl: '/images/artists_jpg.jpg',
+    facebookUrl: 'https://facebook.com',
+    instagramUrl: 'https://instagram.com',
+    youtubeUrl: 'https://youtube.com',
+    videos: [
+      'https://www.youtube.com/embed/DCopcNpzc60',
+      'https://www.youtube.com/embed/oGlDJ3GxvLc',
+    ],
+  },
+  {
+    id: 3,
+    name: 'Saksofonisti Anton Morozov',
+    description: 'Lyhyt kuvaus Anton Morozovista.',
+    imageUrl: '/images/artists_jpg.jpg',
+    facebookUrl: 'https://facebook.com',
+    instagramUrl: 'https://instagram.com',
+    youtubeUrl: 'https://youtube.com',
+    videos: [
+      'https://www.youtube.com/embed/DCopcNpzc60',
+      'https://www.youtube.com/embed/oGlDJ3GxvLc',
+    ],
+  },
+  {
+    id: 4,
+    name: 'Tytti Koivunen',
+    description: 'Lyhyt kuvaus Tytti Koivusesta.',
+    imageUrl: '/images/artists_jpg.jpg',
+    facebookUrl: 'https://facebook.com',
+    instagramUrl: 'https://instagram.com',
+    youtubeUrl: 'https://youtube.com',
+    videos: [
+      'https://www.youtube.com/embed/DCopcNpzc60',
+      'https://www.youtube.com/embed/oGlDJ3GxvLc',
+    ],
+  },
+  {
+    id: 5,
+    name: 'Lotta Virkkunen',
+    description: 'Lyhyt kuvaus Lotta Virkkusesta.',
+    imageUrl: '/images/artists_jpg.jpg',
+    facebookUrl: 'https://facebook.com',
+    instagramUrl: 'https://instagram.com',
+    youtubeUrl: 'https://youtube.com',
+    videos: [
+      'https://www.youtube.com/embed/DCopcNpzc60',
+      'https://www.youtube.com/embed/oGlDJ3GxvLc',
+    ],
+  },
+  {
+    id: 6,
+    name: 'Tanja Vähäsarja',
+    description: 'Lyhyt kuvaus Tanja Vähäsarjasta.',
+    imageUrl: '/images/artists_jpg.jpg',
+    facebookUrl: 'https://facebook.com',
+    instagramUrl: 'https://instagram.com',
+    youtubeUrl: 'https://youtube.com',
+    videos: [
+      'https://www.youtube.com/embed/DCopcNpzc60',
+      'https://www.youtube.com/embed/oGlDJ3GxvLc',
+    ],
+  },
+  {
+    id: 7,
+    name: 'Juontaja Kimmo Oksanen',
+    description: 'Lyhyt kuvaus Kimmo Oksasesta.',
+    imageUrl: '/images/artists_jpg.jpg',
+    facebookUrl: 'https://facebook.com',
+    instagramUrl: 'https://instagram.com',
+    youtubeUrl: 'https://youtube.com',
+    videos: [
+      'https://www.youtube.com/embed/DCopcNpzc60',
+      'https://www.youtube.com/embed/oGlDJ3GxvLc',
+    ],
+  },
+  {
+    id: 8,
+    name: 'Toni Jokiniitty',
+    description: 'Lyhyt kuvaus Toni Jokiniitystä.',
+    imageUrl: '/images/artists_jpg.jpg',
+    facebookUrl: 'https://facebook.com',
+    instagramUrl: 'https://instagram.com',
+    youtubeUrl: 'https://youtube.com',
+    videos: [
+      'https://www.youtube.com/embed/DCopcNpzc60',
+      'https://www.youtube.com/embed/oGlDJ3GxvLc',
+    ],
+  },
+  {
+    id: 9,
+    name: 'Joonas Eloranta',
+    description: 'Lyhyt kuvaus Joonas Elorannasta.',
+    imageUrl: '/images/artists_jpg.jpg',
+    facebookUrl: 'https://facebook.com',
+    instagramUrl: 'https://instagram.com',
+    youtubeUrl: 'https://youtube.com',
+    videos: [
+      'https://www.youtube.com/embed/DCopcNpzc60',
+      'https://www.youtube.com/embed/oGlDJ3GxvLc',
+    ],
+  },
+  {
+    id: 10,
+    name: 'PUSHKIN Quintett',
+    description: 'Lyhyt kuvaus PUSHKIN Quintetista.',
+    imageUrl: '/images/artists_jpg.jpg',
+    facebookUrl: 'https://facebook.com',
+    instagramUrl: 'https://instagram.com',
+    youtubeUrl: 'https://youtube.com',
+    videos: [
+      'https://www.youtube.com/embed/DCopcNpzc60',
+      'https://www.youtube.com/embed/oGlDJ3GxvLc',
+    ],
+  },
+  {
+    id: 11,
+    name: 'Night Shift',
+    description: 'Lyhyt kuvaus Night Shiftistä.',
+    imageUrl: '/images/artists_jpg.jpg',
+    facebookUrl: 'https://facebook.com',
+    instagramUrl: 'https://instagram.com',
+    youtubeUrl: 'https://youtube.com',
+    videos: [
+      'https://www.youtube.com/embed/DCopcNpzc60',
+      'https://www.youtube.com/embed/oGlDJ3GxvLc',
+    ],
+  },
+  {
+    id: 12,
+    name: 'Henriikka Roo',
+    description: 'Lyhyt kuvaus Henriikka Roosta.',
+    imageUrl: '/images/artists_jpg.jpg',
+    facebookUrl: 'https://facebook.com',
+    instagramUrl: 'https://instagram.com',
+    youtubeUrl: 'https://youtube.com',
+    videos: [
+      'https://www.youtube.com/embed/DCopcNpzc60',
+      'https://www.youtube.com/embed/oGlDJ3GxvLc',
+    ],
+  },
+  {
+    id: 13,
+    name: 'Tampereen Ukuleleorkesteri',
+    description: 'Lyhyt kuvaus Tampereen Ukuleleorkesterista.',
+    imageUrl: '/images/artists_jpg.jpg',
+    facebookUrl: 'https://facebook.com',
+    instagramUrl: 'https://instagram.com',
+    youtubeUrl: 'https://youtube.com',
+    videos: [
+      'https://www.youtube.com/embed/DCopcNpzc60',
+      'https://www.youtube.com/embed/oGlDJ3GxvLc',
+    ],
+  },
+]
 
 // Define the route to get all artists
 router.get('/', (req, res) => {
-    res.json(artists);
-});
+  res.json(artists)
+})
 
-module.exports = router;
+// Define the route to get a single artist by ID
+router.get('/:id', (req, res) => {
+  const { id } = req.params
+  const artist = artists.find((a) => a.id === parseInt(id, 10))
+
+  if (artist) {
+    res.json(artist)
+  } else {
+    res.status(404).json({ message: 'Artist not found' })
+  }
+})
+
+module.exports = router
