b2ac7912541eeb699496dd0e56dbd1abd0db225b
Anton Morozov <anton.devaaja@gmail.com>
2025-02-23 17:11:04 +0200

    db.js changed

--- db.js
+++ db.js
@@ -1,11 +1,14 @@
-const mysql = require('mysql2')
+require('dotenv').config();
+const mysql = require('mysql2');
 
-// DB configuration
 const pool = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  database: 'pianoviihde',
-})
+  host: process.env.DB_HOST || 'localhost',
+  user: process.env.DB_USER || 'root',
+  password: process.env.DB_PASS || '',
+  database: process.env.DB_NAME || 'pianoviihde',
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
+});
 
-
-module.exports = pool.promise()
+module.exports = pool.promise();
